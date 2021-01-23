97898545ee01205ec421a91430e8118df8679a96
손정우 <jeson@sonjeong-uui-MacBookAir.local>
2021-01-23 13:01:13 +0900

    bzero -> memset

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jeson <jeson@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 22:49:40 by jeson             #+#    #+#             */
-/*   Updated: 2021/01/21 16:20:48 by jeson            ###   ########.fr       */
+/*   Updated: 2021/01/23 13:00:30 by jeson            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ void		*ft_calloc(size_t cnt, size_t size)
 	total_size = cnt * size;
 	if (!(res = malloc(total_size)))
 		return (NULL);
-	ft_bzero(res, total_size);
+	ft_memset(res, 0, total_size);
 	return (res);
 }
